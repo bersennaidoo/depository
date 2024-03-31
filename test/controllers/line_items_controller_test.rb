c924a4e3560071f13ea47bd53138a7eb74de30c6
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-03-31 11:52:22 +0200

    Implemented cart empty feature and refactored lineitems with quantity attribute

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -22,7 +22,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     follow_redirect!
     assert_select "h2", "Your Techie Sexy Cart"
-    assert_select "li", "Programming Rails 7"
+    assert_select "td", "Programming Rails 7"
   end
 
   test "should show line_item" do
@@ -36,7 +36,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item), params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
